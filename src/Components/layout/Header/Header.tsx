7b8a927078e7c4149e5cb14b9cf2e32dfc06104e
Juan david burgos oquendo <j.dburgos@hotmail.com>
2022-02-24 12:03:28 -0500

    feat: add header styles

--- src/Components/layout/Header/Header.tsx
+++ src/Components/layout/Header/Header.tsx
@@ -1,1 +1,32 @@
-export const Header = () => <header>Header</header>;
+/** React core **/
+import { useState } from 'react';
+
+/** Next core **/
+import Link from 'next/link';
+
+/** Styles **/
+import styles from './Header.module.scss';
+
+export const Header = () => {
+  const [theme, setTheme] = useState('dark');
+
+  const handleThemeSwitch = () => {
+    setTheme(prevState => (prevState === 'light' ? 'dark' : 'light'));
+  };
+
+  const icon = theme === 'dark' ? 'cp-moon-o' : 'cp-sun';
+
+  return (
+    <header className={styles.header}>
+      <span>
+        <Link href="/">
+          <a className={styles.header__title}>Where in the world?</a>
+        </Link>
+      </span>
+      <div className={styles['header__theme-switch']} onClick={handleThemeSwitch}>
+        <i className={icon}></i>
+        <span className={styles['header__theme-switch-text']}>{theme} Mode</span>
+      </div>
+    </header>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>Countries App</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
