fba5e8a7c4a942bb2c343fd905c0d65252cc264a
Juan david burgos oquendo <j.dburgos@hotmail.com>
2022-02-28 08:57:25 -0500

    feat: add filters features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+/** React core **/
+import { useEffect, useState } from 'react';
+
 /** Next core **/
 import type { NextPage } from 'next';
 
@@ -7,6 +10,8 @@ import useSWR from 'swr';
 
 /** Components **/
 import { CountryItem } from '@components/CountryItem';
+
+/** Interfaces **/
 import { ICountry } from '@interfaces/country.interface';
 
 /** Antd **/
@@ -18,22 +23,43 @@ const API = 'https://restcountries.com/v3.1/all';
 
 const Home: NextPage = () => {
   const { data, error } = useSWR(API, fetcher);
+  const [dataCountries, setData] = useState(data);
+
+  useEffect(() => {
+    setData(data);
+  }, [data]);
 
   if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (!data || !dataCountries) return <div>loading...</div>;
 
-  const countries = data.map((country: ICountry, index: number) => (
+  const countries = dataCountries.map((country: ICountry, index: number) => (
     <CountryItem key={index} country={country} />
   ));
 
-  const handleSearch = (value: string) => {
-    if (!value) return;
+  const handleClear = () => {
+    setData(data);
+  };
 
-    console.log(value);
+  const handleSearch = (value: string) => {
+    if (!value) {
+      handleClear();
+
+      return;
+    }
+
+    const dataFiltered = [
+      ...data.filter((country: ICountry) =>
+        country.name.common.toLowerCase().includes(value.toLowerCase()),
+      ),
+    ];
+    setData(dataFiltered);
   };
 
   const handleSelect = (value: string) => {
-    console.log(value);
+    const dataFiltered = [
+      ...data.filter((country: ICountry) => country.region.toLowerCase() === value),
+    ];
+    setData(dataFiltered);
   };
 
   return (
@@ -41,10 +67,10 @@ const Home: NextPage = () => {
       <Search
         className="input"
         placeholder="Search for a country..."
-        onSearch={handleSearch}
         allowClear
         size="large"
         style={{ marginBottom: '30px' }}
+        onSearch={handleSearch}
       />
       <Select
         allowClear
@@ -53,9 +79,10 @@ const Home: NextPage = () => {
         className="select"
         placeholder="Filter by Region"
         onSelect={handleSelect}
+        onClear={handleClear}
       >
         <Option value="africa">Africa</Option>
-        <Option value="america">America</Option>
+        <Option value="americas">Americas</Option>
         <Option value="asia">Asia</Option>
         <Option value="europe">Europe</Option>
         <Option value="oceania">Oceania</Option>
